d4de39e8c8ff981697b4e7f5284ab437ae4ff085
SpectreMelody <stillfeeder@gmail.com>
2023-11-26 21:26:14 +0700

    add canvas network linked

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      display: ['Mulish'] 
+    },
+    colors: {
+      ivory: '#E3E6E0',
+      "primary": '#252930',
+      khaki: '#C0B3A0',
+      "blue-fray": '#6B8490'
+    }
   },
   plugins: [],
 }
