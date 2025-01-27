0d111a1b60b4a487eadd39124a522e58f928985a
SpectreMelody <rudianahadi@gmail.com>
2025-01-27 20:23:52 +0700

    add color when fps drop and battery under 20%

--- docs/assets/js/battery.js
+++ docs/assets/js/battery.js
@@ -1,11 +1,19 @@
-let batteryLevel = document.getElementById("battery-level");
+let batteryGroup = document.getElementById("batteryGroup");
+let batteryLevel = document.getElementById("batteryLevel");
 
 let success = function(battery) {
     if (battery) {
         setStatus();
         function setStatus() {
             batteryLevel.innerHTML = Math.round(battery.level * 100) + "%";
-
+            if(battery.level * 100 > 20)
+            {
+                batteryGroup.classList.remove("text-red-500");
+                batteryGroup.classList.add("text-white");
+            }else{
+                batteryGroup.classList.add("text-red-500");
+                batteryGroup.classList.remove("text-white");
+            }
             battery.addEventListener("levelchange", setStatus, false);
         }
     }
--- docs/assets/js/fps.js
+++ docs/assets/js/fps.js
@@ -3,26 +3,29 @@ let lastTime = performance.now();
 const fpsElement = document.getElementById("fps");
 
 function animate() {
-  // Increment the frame count
   frameCount++;
 
-  // Calculate the time elapsed since the last frame
   const currentTime = performance.now();
   const deltaTime = currentTime - lastTime;
 
-  // If one second has passed, update the FPS display
   if (deltaTime >= 1000) {
-    const fps = frameCount; // FPS is the number of frames counted
+    const fps = frameCount;
     fpsElement.textContent = `FPS: ${fps}`;
 
-    // Reset for the next second
+    if(fps > 20)
+    {
+        fpsElement.classList.remove("text-red-500");
+        fpsElement.classList.add("text-white");
+    }else{
+        fpsElement.classList.add("text-red-500");
+        fpsElement.classList.remove("text-white");
+    }
+
     frameCount = 0;
     lastTime = currentTime;
   }
 
-  // Call the next frame
   requestAnimationFrame(animate);
 }
 
-// Start the animation loop
 animate();
